172cca12cd0f8d36f8a83602db8bde52d70fe61b
Malthe Glent-Madsen <malthe08@hotmail.com>
2021-03-26 12:01:37 +0100

    Implemented bit addition and multiplication

--- MPC/test.go
+++ MPC/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -10,6 +11,18 @@ Test fil til at teste go kode uden at køre hele programmet xd
 
 
 func main() {
+
+	a := []int{0, 1, 0, 1, 0, 0, 1, 1}
+	b := []int{1, 1, 0, 0, 1, 0, 1, 0}
+	fmt.Println(bitMult(a, b))
+	/*
+	a1 := []int{0, 0, 1, 1}
+	a2 := []int{0, 0, 1, 0}
+
+
+	fmt.Println(sliceSubtraction(a1, a2))
+
+	 */
 
 	/*	fmt.Println(calcT(3))
 
@@ -38,6 +51,65 @@ func main() {
 	//fmt.Println(math.Pow(2,3))
 	//fmt.Println(findInverse(-2, 11))
 
+}
+
+func bitAdd(b1 []int, b2 []int) []int {
+
+	bitRes := make([]int, len(b1))
+
+	for i := 0; i < len(b1); i++ {
+		bitRes[i] = b1[i] ^ b2[i]
+	}
+
+	return bitRes
+}
+
+func bitMult(b1, b2 []int) []int {
+	irreducible := makeIrreducible()
+	interRes := make([]int, len(b1) * 2 - 1)
+
+	for i := 0; i < len(b1); i++ {
+		for j := 0; j < len(b2); j++ {
+			interRes[i + j] = interRes[i + j] ^ (b1[i] & b2[j])
+		}
+	}
+
+	for i := 0; i < 7; i++ {
+		if interRes[i] == 1 {
+			sliceSubtraction(interRes, irreducible)
+		}
+
+		irreducible = bitShiftRight(irreducible)
+	}
+
+	interRes = interRes[7:]
+
+	return interRes
+}
+
+func makeIrreducible() []int{
+	irreducible := make([]int, 15)
+	irreducible[8] = 1
+	irreducible[7] = 1
+	irreducible[5] = 1
+	irreducible[4] = 1
+	irreducible[0] = 1
+	return irreducible
+}
+
+func bitShiftRight(array []int) []int {
+	length := len(array)
+	result := make([]int, length)
+	result = append(result[:1], array[: length - 1]...)
+	return result
+}
+
+func sliceSubtraction(a1 []int, a2 []int) []int {
+	for i := 0; i < len(a1); i++ {
+		a1[i] = a1[i] ^ a2[i]
+	}
+
+	return a1
 }
 
 
